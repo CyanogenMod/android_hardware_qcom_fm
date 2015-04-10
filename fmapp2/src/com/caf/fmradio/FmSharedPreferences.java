50c045fe502dbe6d2e4c93849c3c19dbd6ae8008
Edward Wang <ewang@cyngn.com>
2015-04-09 17:22:50 -0700

    FM: always use overlay default country
    Change-Id: I2acbbde684e7581124577f6e929397279d97cb41
    (cherry picked from commit 087fb5a9128f8a9d96289815b1ae4be2ea98981a)

--- fmapp2/src/com/caf/fmradio/FmSharedPreferences.java
+++ fmapp2/src/com/caf/fmradio/FmSharedPreferences.java
@@ -169,6 +169,8 @@ public class FmSharedPreferences
    private static int mRecordDuration = 0;
    private static int mLastAudioMode = -1;
 
+   public static int mDefaultCountryIndex = REGIONAL_BAND_NORTH_AMERICA;
+
    FmSharedPreferences(Context context){
       mContext = context.getApplicationContext();
       mFMConfiguration = new FmConfig();
@@ -500,8 +502,8 @@ public class FmSharedPreferences
       if (Locale.getDefault().equals(Locale.CHINA)) {
           setCountry(sp.getInt(FMCONFIG_COUNTRY, REGIONAL_BAND_CHINA));
       } else {
-          int defaultIndex = mContext.getResources().getInteger(R.integer.default_country_index);
-          setCountry(sp.getInt(FMCONFIG_COUNTRY, defaultIndex));
+          mDefaultCountryIndex = mContext.getResources().getInteger(R.integer.default_country_index);
+          setCountry(sp.getInt(FMCONFIG_COUNTRY, mDefaultCountryIndex));
       }
       /* Last list the user was navigating */
       mListIndex = sp.getInt(LAST_LIST_INDEX, 0);
@@ -575,7 +577,7 @@ public class FmSharedPreferences
           setCountry(REGIONAL_BAND_CHINA);
           //Others set north America.
       } else {
-          setCountry(REGIONAL_BAND_NORTH_AMERICA);
+          setCountry(mDefaultCountryIndex);
       }
    }
 
