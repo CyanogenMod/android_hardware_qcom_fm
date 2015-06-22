79175056350b6e73d7737d8273c55a1a3356d18c
Martin Brabham <optedoblivion@cyngn.com>
2015-06-22 11:20:26 -0700

    Fix status text to reflect when FM is enabled.
    Change-Id: Idbb39cdd8c6eb016d4644d9e31290507d17ce820

--- fmapp2/src/com/caf/fmradio/FMRadio.java
+++ fmapp2/src/com/caf/fmradio/FMRadio.java
@@ -2024,7 +2024,11 @@ public class FMRadio extends Activity
          }
       }else {
          if (mRadioTextTV != null) {
-             mRadioTextTV.setText(getString(R.string.fm_off));
+             if (bEnable) {
+                 mRadioTextTV.setText("");
+             } else {
+                 mRadioTextTV.setText(getString(R.string.fm_off));
+             }
              mRadioTextScroller.mOriginalString = "";
          }
          if (mERadioTextTV != null) {
