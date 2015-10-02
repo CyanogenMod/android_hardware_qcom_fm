77250415430fdf2b501b4ed32a279e7b82d06f80
Ed Falk <efalk@cyngn.com>
2015-10-01 17:59:09 -0700

    FMRadio : Fix settings UI bugs
    Setting any user-defined parameter caused the band to switch
    from user-defined to Indonesia. Fixed.
    Also removed some bogus localization while I was there.
    
    Issue-Id: CYNGNOS-778
    Change-Id: I256f04eab39b820b33676bf598322d480d6dfa44
    (cherry picked from commit 0a805b5822d7e0de19c88aba38339d4ac65be876)

--- fmapp2/src/com/caf/fmradio/Settings.java
+++ fmapp2/src/com/caf/fmradio/Settings.java
@@ -144,6 +144,7 @@ public class Settings extends PreferenceActivity implements
            mUserBandMinPref.getEditText().setInputType(InputType.TYPE_CLASS_NUMBER |
                                                         InputType.TYPE_NUMBER_FLAG_DECIMAL);
            mUserBandMinPref.setDialogTitle(R.string.usr_def_band_min);
+           mUserBandMinPref.getEditText().setSelectAllOnFocus(true);
 
            mUserBandMaxPref = new EditTextPreference(this);
            mUserBandMaxPref.setKey(USER_DEFINED_BAND_MAX_KEY);
@@ -151,6 +152,7 @@ public class Settings extends PreferenceActivity implements
            mUserBandMaxPref.setDialogTitle(R.string.usr_def_band_max);
            mUserBandMaxPref.getEditText().setInputType(InputType.TYPE_CLASS_NUMBER |
                                                         InputType.TYPE_NUMBER_FLAG_DECIMAL);
+           mUserBandMaxPref.getEditText().setSelectAllOnFocus(true);
 
            setBandSummary(index);
 
@@ -331,14 +333,10 @@ public class Settings extends PreferenceActivity implements
                if((freq > 0) && (freq < max_freq) && (freq >= 76000) && (noOfChannels > 0)) {
                   FmSharedPreferences.setLowerLimit((int)freq);
                   sendSettingsChangedIntent(FM_BAND_CHANGED);
-                  setBandSummary(summaryBandItems.length - 1);
+                  setBandSummary(FmSharedPreferences.REGIONAL_BAND_USER_DEFINED);
                   clearStationList();
                }else {
-                  if ((Locale.getDefault().toString().equals("zh_HK")))
-                      Toast.makeText(this,"請輸入有效的頻道範圍76.0-108.0",
-                                                            Toast.LENGTH_SHORT).show();
-                  else
-                      Toast.makeText(this, getString(R.string.user_defind_band_msg),
+                  Toast.makeText(this, getString(R.string.user_defind_band_msg),
                                                             Toast.LENGTH_SHORT).show();
                }
            }else if(key.equals(USER_DEFINED_BAND_MAX_KEY)) {
@@ -357,14 +355,10 @@ public class Settings extends PreferenceActivity implements
                if((freq > 0) && (freq > min_freq) && (freq <= 108000) && (noOfChannels > 0)) {
                   FmSharedPreferences.setUpperLimit((int)freq);
                   sendSettingsChangedIntent(FM_BAND_CHANGED);
-                  setBandSummary(summaryBandItems.length - 1);
+                  setBandSummary(FmSharedPreferences.REGIONAL_BAND_USER_DEFINED);
                   clearStationList();
                }else {
-                  if ((Locale.getDefault().toString().equals("zh_HK")))
-                      Toast.makeText(this,"請輸入有效的頻道範圍76.0-108.0",
-                                                            Toast.LENGTH_SHORT).show();
-                  else
-                      Toast.makeText(this, getString(R.string.user_defind_band_msg),
+                  Toast.makeText(this, getString(R.string.user_defind_band_msg),
                                                             Toast.LENGTH_SHORT).show();
                }
           }else {
